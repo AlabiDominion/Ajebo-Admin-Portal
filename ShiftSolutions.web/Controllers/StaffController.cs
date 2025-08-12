5b601530700ba12e76a9f6fd1514beb31a327745
AlabiDominion <alabidominion@gmail.com>
2025-08-12 23:31:27 +0100

    Completed Dashboard Page

---
+++ ShiftSolutions.web/Controllers/StaffController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShiftSolutions.web.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult ListStaff()
+        {
+            return View();
+        }
+        public IActionResult NewStaff()
+        {
+            return View();
+        }
+    }
+}
